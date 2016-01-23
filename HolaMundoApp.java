47b5df17fe49319d80fca0386aeb489b6e26c28c
ComradeMudkipz <comrademudkipz@live.com>
2016-01-23 05:36:50 -0800

    Initial commit

---
+++ HolaMundoApp.java
@@ -0,0 +1,9 @@
+/**
+ * The HolaMundoApp class implements an application that
+ * simply prints "Hola Mundo!" to standard output.
+ */
+class HolaMundoApp {
+    public static void main(String [] args) {
+        System.out.println("Hola World!"); // Display the string
+    }
+}
